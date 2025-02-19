72886cc62d2e456b1347a38bb911fa7710642e80
Michael Rack <rack@Michaels-MacBook-Pro.local>
2025-02-19 16:23:14 -0500

    add test ci script

---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  SQDemo
+#
+#  Created by Michael Rack on 2/19/25.
+#
+  
+  # extract coverage data from project using xcode native tool
+  
+  xccov-to-sonarqube-generic.sh $CI_RESULT_BUNDLE_PATH/.*XCResult > AAAAA.xml
+  cat AAAAA.xml
+  # run sonar scanner and upload coverage data with the current app version
+  #TODO: Set up my api key? project name? What do we need here
+  #sonar-scanner \
+    #-Dsonar.projectVersion=${APP_VERSION}
