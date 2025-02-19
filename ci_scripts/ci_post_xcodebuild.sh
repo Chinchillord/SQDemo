525422a6b1d7e97e32768564cc8f2d172cbcb73f
Michael Rack <rack@Michaels-MacBook-Pro.local>
2025-02-19 17:31:10 -0500

    add sonarqube steps

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -7,10 +7,12 @@
 #
   
   # extract coverage data from project using xcode native tool
-  
+  brew install sonar-scanner
   bash xccov-to-sonarqube-generic.sh /Volumes/workspace/*.xcresult > AAAAA.xml
   cat AAAAA.xml
-  # run sonar scanner and upload coverage data with the current app version
-  #TODO: Set up my api key? project name? What do we need here
-  #sonar-scanner \
-    #-Dsonar.projectVersion=${APP_VERSION}
+sonar-scanner \
+  -Dsonar.organization=chinchillord \
+  -Dsonar.projectKey=Chinchillord_SQDemo \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io
+  -Dsonar.coverageReportPaths=AAAAA.xml
