dbaceb19d8bdb05a1fb0829398137e13287b571f
Michael Rack <rack@Michaels-MacBook-Pro.local>
2025-03-12 19:38:01 -0400

    reverting scm provider

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -10,17 +10,13 @@
 if [ "$CI_XCODEBUILD_ACTION" = "test-without-building" ]
 then
     brew install sonar-scanner
-    ls -la $CI_WORKSPACE/.git
-    git status
-    export PATH="/usr/bin:/bin:/usr/sbin:/sbin"
-    git status
     bash xccov-to-sonarqube-generic.sh /Volumes/workspace/*.xcresult > AAAAA.xml
     cat AAAAA.xml
-    cd $(git rev-parse --show-toplevel)
     sonar-scanner \
     -Dsonar.organization=chinchillord \
     -Dsonar.projectKey=Chinchillord_SQDemo \
     -Dsonar.sources=. \
     -Dsonar.host.url=https://sonarcloud.io \
-    -Dsonar.coverageReportPaths=AAAAA.xml
+    -Dsonar.coverageReportPaths=AAAAA.xml \
+    -Dsonar.scm.provider=git
 fi
