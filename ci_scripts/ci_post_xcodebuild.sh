3c0b6b294289f2a96dbeec11e077becd26d5eae6
benpatterson48 <ben.patterson@underdogfantasy.com>
2025-04-28 22:06:58 -0500

    Disable scm

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -18,5 +18,7 @@ then
     -Dsonar.sources=. \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.coverageReportPaths=AAAAA.xml \
-    -Dsonar.scm.provider=git
+    #-Dsonar.scm.provider=git
+    # Trying to scm disabled which removes git blame/new code detection but just seeing if this removes the issue
+    -Dsonar.scm.disabled=true
 fi
