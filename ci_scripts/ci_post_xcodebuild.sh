be8260a2637412b88b3844a2cc6fca093463ef05
benpatterson48 <ben.patterson@underdogfantasy.com>
2025-04-30 13:49:06 -0500

    More script changes

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -12,12 +12,15 @@ then
     brew install sonar-scanner
     bash xccov-to-sonarqube-generic.sh /Volumes/workspace/*.xcresult > AAAAA.xml
     cat AAAAA.xml
+    echo "==> Listing contents under expected test paths:"
+    find /Volumes/workspace/repository/SQDemoTests -type f
+    find /Volumes/workspace/repository/SQDemoUITests -type f
     sonar-scanner \
       -Dsonar.projectBaseDir=/Volumes/workspace/repository \
       -Dsonar.organization=chinchillord \
       -Dsonar.projectKey=Chinchillord_SQDemo \
-      -Dsonar.sources=SQDemo \
-      -Dsonar.tests=SQDemo/SQDemoTests,SQDemo/SQDemoUITests \
+      -Dsonar.sources=. \
+      -Dsonar.tests=SQDemoTests,SQDemoUITests \
       -Dsonar.host.url=https://sonarcloud.io \
       -Dsonar.coverageReportPaths=ci_scripts/AAAAA.xml \
       -Dsonar.scm.disabled=true
