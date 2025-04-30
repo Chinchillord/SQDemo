a27e98eaf8479dac6745ee07b65a73e726dbd34b
benpatterson48 <ben.patterson@underdogfantasy.com>
2025-04-30 16:12:17 -0500

    Create and force git clone in xcodecloud workflow file

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -12,8 +12,10 @@ then
     brew install sonar-scanner
     bash xccov-to-sonarqube-generic.sh /Volumes/workspace/*.xcresult > AAAAA.xml
     cat AAAAA.xml
-    echo "==> Tree dump of repository:"
-    find /Volumes/workspace/repository -type d | sort
+    echo "==> Full repository dump:"
+    find /Volumes/workspace/repository -print
+    echo "==> Tree dump of repo (all files):"
+    find /Volumes/workspace/repository | sort
     echo "==> Listing contents under expected test paths:"
     find /Volumes/workspace/repository/SQDemoTests -type f
     find /Volumes/workspace/repository/SQDemoUITests -type f
@@ -21,7 +23,7 @@ then
       -Dsonar.projectBaseDir=/Volumes/workspace/repository \
       -Dsonar.organization=chinchillord \
       -Dsonar.projectKey=Chinchillord_SQDemo \
-      -Dsonar.sources=. \
+      -Dsonar.sources=SQDemo \
       -Dsonar.tests=SQDemoTests,SQDemoUITests \
       -Dsonar.host.url=https://sonarcloud.io \
       -Dsonar.coverageReportPaths=ci_scripts/AAAAA.xml \
