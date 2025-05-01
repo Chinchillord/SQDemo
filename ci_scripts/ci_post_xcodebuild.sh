6cda3a3514f2f02800d7d2651e9d331566263fba
benpatterson48 <ben.patterson@underdogfantasy.com>
2025-05-01 16:23:09 -0500

    Project Setup

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -6,19 +6,23 @@
 #  Created by Michael Rack on 2/19/25.
 #
   
-  # extract coverage data from project using xcode native tool
-if [ "$CI_XCODEBUILD_ACTION" = "test-without-building" ]
-then
+if [ "$CI_XCODEBUILD_ACTION" = "test" ]
+then      
     brew install sonar-scanner
     bash xccov-to-sonarqube-generic.sh /Volumes/workspace/*.xcresult > AAAAA.xml
     cat AAAAA.xml
+    echo "==> Full repository dump:"
+    find /Volumes/workspace/repository -print
+    echo "==> Tree dump of repo (all files):"
+    find /Volumes/workspace/repository | sort
+    echo "==> Listing contents under expected test paths:"
+    find /Volumes/workspace/repository/SQDemoTests -type f
+    find /Volumes/workspace/repository/SQDemoUITests -type f
     sonar-scanner \
-      -Dsonar.projectBaseDir=/Volumes/workspace/repository \
-      -Dsonar.organization=chinchillord \
-      -Dsonar.projectKey=Chinchillord_SQDemo \
-      -Dsonar.sources=SQDemo \
-      -Dsonar.tests=SQDemoTests,SQDemoUITests \
+      -Dsonar.organization=benpatterson48 \
+      -Dsonar.projectKey=benpatterson48_SQDemo \
+      -Dsonar.sources=. \
       -Dsonar.host.url=https://sonarcloud.io \
-      -Dsonar.coverageReportPaths=ci_scripts/AAAAA.xml \
+      -Dsonar.coverageReportPaths=AAAAA.xml \
       -Dsonar.scm.disabled=true
 fi
