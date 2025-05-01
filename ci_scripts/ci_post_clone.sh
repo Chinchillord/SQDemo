8f1b902196dd6d9397d8da2d9a8e5ab7e2473642
benpatterson48 <ben.patterson@underdogfantasy.com>
2025-05-01 13:04:53 -0500

    Changes

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-if [ "$CI_WORKFLOW" = "UnitTests - iOS" ]
-then
   # fetch a reference to the develop branch on GitHub
   # this will allow SonarQube analysis to work
   git -C $CI_WORKSPACE checkout -b temp
@@ -13,6 +11,3 @@ then
   git -C $CI_WORKSPACE config remote.origin.fetch \
     "+refs/heads/$CI_PULL_REQUEST_TARGET_BRANCH:refs/remotes/origin/$CI_PULL_REQUEST_TARGET_BRANCH"
   git -C $CI_WORKSPACE fetch
-else
-  echo "==> Skipping Git ref restoration"
-fi
